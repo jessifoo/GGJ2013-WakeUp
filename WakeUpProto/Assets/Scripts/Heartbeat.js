6bc15c8ab5e64d13d1c36203f20b7c2de39fb2c8
Jessica Johnson <jessykarenman@gmail.com>
2013-01-26 14:19:07 -0800

    Heartbeat increases and decreases slower (although still linearly). Longer path in limbo1

--- WakeUpProto/Assets/Scripts/Heartbeat.js
+++ WakeUpProto/Assets/Scripts/Heartbeat.js
@@ -15,31 +15,53 @@ var SPEED_THRESHOLD_INCREASE : float = 4.0;
 var HEARTBEAT_TIME_UNTIL_DECREASE : float = 1.0; //Seconds until the players heartbeat will decrease
 var heartbeat_decrease_countdown : float;
 
+var deltaHeartbeat : float;
 
+// Player attributes
+var player : GameObject;
+var playerMovement : CharacterMotorMovement;
+var playerSpeed : float;
+var maxSpeed : float;
 
-
-
-function FixedUpdate () {
-	var player : GameObject = GameObject.FindGameObjectsWithTag("Player")[0];
-	var playerMovement = player.GetComponentInChildren(CharacterMotor).movement;
+function awake() {
 	
+}
+
+function FixedUpdate () { 
+	player = GameObject.FindGameObjectsWithTag("Player")[0];
+	playerMovement = player.GetComponentInChildren(CharacterMotor).movement;
+	playerSpeed = playerMovement.velocity.magnitude;
+	maxSpeed = player.GetComponent(CharacterMotor).movement.maxForwardSpeed;
+	
+	
+	//deltaHeartbeat = Mathf.Tan((playerSpeed / (maxSpeed/2)) - 1);
 	//Heartbeat is decreasing:
+	
 	if ( playerMovement.velocity.magnitude < SPEED_THRESHOLD_DECREASE ) {
+		
 		heartbeat_decrease_countdown -= Time.fixedDeltaTime;
 		if ( heartbeat_decrease_countdown <= 0 ){
-			//Decrease the heartbeat!
-			heartbeat -= 1;
+			// Decrease the heartbeat!
+			 heartbeat -= 0.1;
+			// heartbeat -= Mathf.Exp(Time.fixedDeltaTime);
+			//heartbeat = Mathf.Tan((playerSpeed/(maxSpeed/2))-1);
 		}
 	} else {
 		heartbeat_decrease_countdown = HEARTBEAT_TIME_UNTIL_DECREASE;
 	}
-	//Heartbeat is increasing:
+	
+	// Heartbeat is increasing:
+	
 	if ( playerMovement.velocity.magnitude > SPEED_THRESHOLD_INCREASE ) {
 		heartbeat += hb_stepSize;
-		hb_stepSize *= HB_STEP_SIZE_ACCEL;
+		//hb_stepSize *= HB_STEP_SIZE_ACCEL;
 	} else {
 		hb_stepSize = 0.1;
 	}
+	
+	
+	//heartbeat = deltaHeartbeat * Time.fixedDeltaTime * 1000;
+	
 	
 	if ( heartbeat <= MIN_HEARTBEAT ) {
 		heartbeat = MIN_HEARTBEAT;
@@ -59,6 +81,7 @@ function FixedUpdate () {
 
 //Debug - draw the heartbeat on the gui
 function OnGUI () {
-	GUI.TextArea(new Rect(10, 10, 20, 20), "" + heartbeat);
+	GUI.TextArea(new Rect(10, 10, 30, 20), "" + heartbeat);
+	GUI.TextArea(new Rect(40, 10, 80, 20), "" + playerSpeed);
 }
 
