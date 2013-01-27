f352a9b2bef82a432456902ee002c36124f2937d
Jessica Johnson <jessykarenman@gmail.com>
2013-01-27 09:28:56 -0800

    Instructions, credits, filled out

--- WakeUpProto/Assets/Scripts/Instructions.js
+++ WakeUpProto/Assets/Scripts/Instructions.js
@@ -2,6 +2,17 @@
 
 // The position on of the scrolling viewport
 var scrollPosition : Vector2 = Vector2.zero;
+var instructions : GUIText;
+var instructions1 : GUIText;
+var instructions2 : GUIText;
+var instructions3 : GUIText;
+var instructions4 : GUIText;
+var instructions5 : GUIText;
+var instructions6 : GUIText;
+var instructions7: GUIText;
+var instructions8: GUIText;
+var instructions9: GUIText;
+var instructions10: GUIText;
 
 function Start () {
 
@@ -12,10 +23,18 @@ function Update () {
 }
 
 function OnGUI () {
+	if (GUI.Button(Rect(((Screen.width/2)-(Screen.width/2.5)),Screen.height-40,200,30),"Back")){
+    		Application.LoadLevel ("Start");
+    	}
+    	
+    if (GUI.Button(Rect((Screen.width/2),Screen.height-40,200,30),"Start")){
+    		Application.LoadLevel ("Graveyard");
+    	} 
+    	
     // An absolute-positioned example: We make a scrollview that has a really large client
     // rect and put it in a small rect on the screen.
-    scrollPosition = GUI.BeginScrollView (Rect (20,20,(Screen.width-20),(Screen.height-20)),
-        scrollPosition, Rect (0, 0, Screen.width-40,Screen.height+100));
+    //scrollPosition = GUI.BeginScrollView (Rect (20,20,(Screen.width-20),(Screen.height-20)),
+    //    scrollPosition, Rect (0, 0, Screen.width-40,Screen.height+100));
     
     // Make four buttons - one in each corner. The coordinate system is defined
     // by the last parameter to BeginScrollView.
@@ -25,10 +44,6 @@ function OnGUI () {
     GUI.Button (Rect (0,180,100,20), "Bottom-left");
     GUI.Button (Rect (120,180,100,20), "Bottom-right");
     */
-   // GUI.TextArea( Rect (0,0,Screen.width-50,Screen.height-50), 
-    //"You are in a coma. You live in a world at war with evil forces that want to enslave the minds of all mankind. You must traverse through your subconcious, battleing your worst fears, in an attempt to come back to conciousness");
-    GUI.Label( Rect (0,0,Screen.width-50,Screen.height-50), 
-    "You are in a coma. You live in a world at war with evil forces that want to enslave the minds of all mankind. You must traverse through your subconcious, battleing your worst fears, in an attempt to come back to conciousness");
-    // End the scroll view that we began above.
-    GUI.EndScrollView ();
+     // End the scroll view that we began above.
+    //GUI.EndScrollView ();
 }
--- WakeUpProto/Assets/Scripts/Maze.js
+++ WakeUpProto/Assets/Scripts/Maze.js
@@ -11,7 +11,7 @@ function Start () {
 
 function OnGUI() {
    if( bool == false){
-     	if (GUI.Button(Rect(325,300,200,30),"Enough with the Maze, Move on")){
+     	if (GUI.Button(Rect(Screen.width-220,Screen.height-40,200,30),"Enough with the Maze, Move on")){
     		GameObject.FindWithTag("Player").transform.position = new Vector3(65,130,80);
     		bool = true;
     	}    
