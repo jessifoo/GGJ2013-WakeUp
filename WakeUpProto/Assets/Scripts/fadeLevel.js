ad8810cbdac7eebb4d6cfe0930bfdf095168c1d6
Jessica Johnson <jessykarenman@gmail.com>
2013-01-27 10:36:56 -0800

    level fades fixed

--- WakeUpProto/Assets/Scripts/fadeLevel.js
+++ WakeUpProto/Assets/Scripts/fadeLevel.js
@@ -12,21 +12,18 @@ function Update () {
 }
 
 function OnTriggerEnter(other : Collider) {
-	if(currentLevel == 0){
-		nextLevel = 1;
-	}else if(currentLevel == 1){
+	if(currentLevel == 1){
 		nextLevel = 2;
 	}else if(currentLevel == 2){
 		nextLevel = 3;
-	}
-	
-	/*else if(currentLevel == 3){
+	}else if(currentLevel == 3){
 		nextLevel = 4;
+	}else if(currentLevel == 4){
+		nextLevel = 5;
+	}else if(currentLevel == 5){
+		nextLevel = 6;
 	}
-	//else if(currentLevel == 4){
-		//nextLevel = 5;
-	//}
-*/
+
 	if(other.gameObject.tag == "Player"){
 		LevelLoadFade.FadeAndLoadLevel(nextLevel, Color.white, 2.0);
 	}
