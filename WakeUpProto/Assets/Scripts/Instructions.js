d22c245408d7bbe9e6a34eb3b0649e6c449649e0
Jessica Johnson <jessykarenman@gmail.com>
2013-01-27 07:20:41 -0800

    instructions scene added

--- WakeUpProto/Assets/Scripts/Instructions.js
+++ WakeUpProto/Assets/Scripts/Instructions.js
@@ -14,16 +14,21 @@ function Update () {
 function OnGUI () {
     // An absolute-positioned example: We make a scrollview that has a really large client
     // rect and put it in a small rect on the screen.
-    scrollPosition = GUI.BeginScrollView (Rect (10,10,1000,1000),
-        scrollPosition, Rect (0, 0, 220, 200));
+    scrollPosition = GUI.BeginScrollView (Rect (20,20,(Screen.width-20),(Screen.height-20)),
+        scrollPosition, Rect (0, 0, Screen.width-40,Screen.height+100));
     
     // Make four buttons - one in each corner. The coordinate system is defined
     // by the last parameter to BeginScrollView.
+    /* 
     GUI.Button (Rect (0,0,100,20), "Top-left");
     GUI.Button (Rect (120,0,100,20), "Top-right");
     GUI.Button (Rect (0,180,100,20), "Bottom-left");
     GUI.Button (Rect (120,180,100,20), "Bottom-right");
-    
+    */
+   // GUI.TextArea( Rect (0,0,Screen.width-50,Screen.height-50), 
+    //"You are in a coma. You live in a world at war with evil forces that want to enslave the minds of all mankind. You must traverse through your subconcious, battleing your worst fears, in an attempt to come back to conciousness");
+    GUI.Label( Rect (0,0,Screen.width-50,Screen.height-50), 
+    "You are in a coma. You live in a world at war with evil forces that want to enslave the minds of all mankind. You must traverse through your subconcious, battleing your worst fears, in an attempt to come back to conciousness");
     // End the scroll view that we began above.
     GUI.EndScrollView ();
 }
