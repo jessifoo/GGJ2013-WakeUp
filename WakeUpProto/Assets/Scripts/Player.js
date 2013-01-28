d3738264540e66fd40abf92defb4773b0d6f5dd1
Ian W Fraser <iwfraser.code@gmail.com>
2013-01-28 15:54:18 -0800

    Added cursor lock and right clicking in web build.
    Use escape to unlock the cursor.

--- WakeUpProto/Assets/Scripts/Player.js
+++ WakeUpProto/Assets/Scripts/Player.js
@@ -25,6 +25,7 @@ private var vignette : Vignetting;
 private var bareHand : BareHand;
 
 function Awake () {
+	Screen.lockCursor = true;
 	// Weapons
 	bareHand = GetComponentInChildren(BareHand);
 	
@@ -45,6 +46,8 @@ function Awake () {
 private var playerMaxSpeedWalking : float;
 var playerMaxSpeedRunning : float;
 function FixedUpdate () { 
+	CheckMouseLocking();
+	
 	//Controls added on:
 	if ( Input.GetKey(KeyCode.LeftShift) ) {
 		playerMovement.maxForwardSpeed   = playerMaxSpeedRunning;
@@ -140,5 +143,45 @@ function OnGUI () {
 }
 
 
+// Mouse Locking
+private var wasLocked = false;
+function CheckMouseLocking() {
+	// In standalone player we have to provide our own key
+    // input for unlocking the cursor
+    if (Input.GetKeyDown ("escape")) {	// Currently only locks the cursor
+        Screen.lockCursor = false;
+    }
+    
+    // Did we lose cursor locking?
+    // eg. because the user pressed escape
+    // or because he switched to another application
+    // or because some script set Screen.lockCursor = false;
+    if (!Screen.lockCursor && wasLocked) {
+        wasLocked = false;
+        DidUnlockCursor();
+    }
+    // Did we gain cursor locking?
+    else if (Screen.lockCursor && !wasLocked) {
+        wasLocked = true;
+        DidLockCursor ();
+    }
+}
 
+function OnMouseDown () {
+    // Lock the cursor
+    Screen.lockCursor = true;
+}
+
+// Called when the cursor is actually being locked
+function DidLockCursor () {
+    Debug.Log("Locking cursor");
+
+}
+
+// Called when the cursor is being unlocked
+// or by a script calling Screen.lockCursor = false;
+function DidUnlockCursor () {
+    Debug.Log("Unlocking cursor");
+
+}
 
